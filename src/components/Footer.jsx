07baf631f48b5c46ae7bf51c5b1a3faff4955223
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-07-18 19:39:38 +0700

    add header, footer, and note components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright {currentYear} © https://github.com/ditramadia</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+function Note() {
+    return (
+        <div className="note">
+            <h1>Test</h1>
+            <p>Content</p>
+        </div>
+    );
+}
+
+export default Note;
